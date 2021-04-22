93b8afdbcee13f8d5222abec734a5282639b6b87
Chromium7 <christofersaputra29@gmail.com>
2021-04-22 07:35:05 +0700

    Add tailwind css dependencies

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,9 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Posty</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
-<body>
+<body class="bg-gray-100">
+    <nav class="p-6 bg-white flex justify-between">
+        <ul class="flex items-center">
+            <li><a href="">Home</a></li>
+            <li><a href="">Dashboard</a></li>
+            <li><a href="">Post</a></li>
+
+        </ul>
+    </nav>
     @yield('content')
 </body>
 
