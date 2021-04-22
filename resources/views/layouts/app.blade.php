a557a9b453ed9caa61a740c47340a670dbc42496
Chromium7 <christofersaputra29@gmail.com>
2021-04-22 11:48:18 +0700

    Add login controller and view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,10 +17,14 @@
             <li><a href="" class="p-3">Post</a></li>
         </ul>
         <ul class="flex items-center">
-            <li><a href="" class="p-3">Alex Smith</a></li>
-            <li><a href="" class="p-3">Login</a></li>
-            <li><a href="{{ route('register') }}" class="p-3">Register</a></li>
-            <li><a href="" class="p-3">Logout</a></li>
+            @auth
+                <li><a href="" class="p-3">Alex Smith</a></li>
+                <li><a href="" class="p-3">Logout</a></li>
+            @endauth
+            @guest
+                <li><a href="{{ route('login') }}" class="p-3">Login</a></li>
+                <li><a href="{{ route('register') }}" class="p-3">Register</a></li>
+            @endguest
         </ul>
     </nav>
     @yield('content')
