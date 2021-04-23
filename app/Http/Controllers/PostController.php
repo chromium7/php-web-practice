b18b9c55abaf1bd4ed9acf3d6c69751cf509f211
Chromium7 <christofersaputra29@gmail.com>
2021-04-23 20:57:08 +0700

    Add to post controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,16 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::get();
+        return view('posts.index', [
+            'posts' => $posts
+        ]);
     }
 
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'body' => 'required'
+        ]);
+
+        $request->user()->posts()->create([$request->only('body')]);
+
+        // Post::create([
+        //     'user_id' => auth()->id(),
+        //     'body' => $request->body
+        // ]);
+
+        return back();
     }
 }
